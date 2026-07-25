69fa49ed41de08679364fac36ec21df73181c8eb
Baim Warunk Arsi <admin@baimwarunkarsi.com>
2026-07-25 12:21:25 +0700

    fix: Aktifkan app.use(cors()) agar GitHub Pages diizinkan mengakses API Vercel

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const PORT = 3000;
 const ADMIN_PASSWORD = 'baimdigital2026';
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(process.cwd(), '.'), { extensions: ['html'] }));
